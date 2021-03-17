c058886e765175bcbfacb6a46618efe99f0097aa
Harsh Rawat <rawahars@amazon.com>
2021-03-17 20:06:56 +0000

    Adding a few more fields.

--- main.go
+++ main.go
@@ -5,29 +5,36 @@ import (
 	_ "plugin"
 )
 
-const(
+const (
 	Add = true
 	//Add = false
-	networkNameENI = "task_br"
-	addressPrefix = "10.0.0.159/30"
-	ipAddr = "10.0.0.159"
+	networkNameENI = "task_br2"
+	network2       = "taskbr02f17460b6c1"
+	addressPrefix  = "10.0.0.228/24"
+	ipAddr         = "10.0.0.228"
 
-	containerID1 = "84a8b335171e246d6afd833324ad0fa1f0c6a549d00773b5292dc38cd9e1c161"
-	containerID2 = ""
-	endpointName = "task-eni-ep"
+	adapterName = "Ethernet 6"
 
+	containerID1    = "bd4fc4c9beb00070b86b78e7fe33cf9d340b6998f8e9338c1ad897b290bcf607" //"0a6940de65e0c11cf051cdd9d657e7dd89763ca5ca0b13551fffb7754a18119c"
+	containerID2    = "328575d08fe2266f48a630f81ceabcb69b7a8c2f3383afecb3a46da262ee61f2" //"fd95ce38dbf4a35243edb8fc80d820ec3bfc0d3e249e89932aeb45808d13d891"
+	endpointName    = "task-eni-ep2"
+	natEndpointName = "nat-cid-12345"
 
 	networkNameNAT = "nat"
-	gateway = "10.0.0.1"
+	gateway        = "10.0.0.1"
 )
 
-func main(){
+func main() {
 	if Add {
-		CreateNetwork(networkNameENI, addressPrefix, gateway)
+		CreateNetwork(networkNameENI, addressPrefix, gateway, adapterName)
+		//CreateNetwork(networkNameNAT, addressPrefix, gateway)
 		fmt.Println("Creating Endpoint now")
-		CreateEndpoint(containerID1, true, endpointName, networkNameENI, gateway, ipAddr)
+		CreateEndpoint(containerID1, true, endpointName, networkNameENI, gateway, ipAddr, false)
+		CreateEndpoint(containerID2, false, endpointName, networkNameENI, gateway, ipAddr, false)
+		//CreateEndpoint(containerID1, true, natEndpointName, networkNameNAT, gateway, ipAddr, true)
 		fmt.Println("\nEndpoint Created and connected")
 	} else {
 		DeleteNetwork(networkNameENI)
+		//DeleteNetwork(network2)
 	}
 }
--- plugin.go
+++ plugin.go
@@ -14,7 +14,7 @@ type hnsRoutePolicy struct {
 	NeedEncap         bool   `json:"NeedEncap,omitempty"`
 }
 
-func CreateNetwork(networkName string, addressPrefix string, gateway string){
+func CreateNetwork(networkName string, addressPrefix string, gateway string, adapterName string) {
 	hnsNetwork, err := hcsshim.GetHNSNetworkByName(networkName)
 
 	if err == nil {
@@ -25,7 +25,7 @@ func CreateNetwork(networkName string, addressPrefix string, gateway string){
 		hnsNetwork = &hcsshim.HNSNetwork{
 			Name:               networkName,
 			Type:               "l2bridge",
-			NetworkAdapterName: "",
+			NetworkAdapterName: adapterName,
 
 			Subnets: []hcsshim.Subnet{
 				{
@@ -52,71 +52,78 @@ func CreateNetwork(networkName string, addressPrefix string, gateway string){
 	}
 }
 
-func DeleteNetwork(networkName string){
+func DeleteNetwork(networkName string) {
 	// Find the HNS network ID.
 	hnsNetwork, err := hcsshim.GetHNSNetworkByName(networkName)
 	if err != nil {
-		fmt.Printf("Error while network detection %s", err)
+		fmt.Printf("Error while network detection %s\n", err)
 		return
 	}
 
 	// Delete the HNS network.
-	fmt.Printf("Deleting HNS network name: %s ID: %s.", networkName, hnsNetwork.Id)
+	fmt.Printf("Deleting HNS network name: %s ID: %s.\n", networkName, hnsNetwork.Id)
 	_, err = hcsshim.HNSNetworkRequest("DELETE", hnsNetwork.Id, "")
 	if err != nil {
-		fmt.Printf("Failed to delete HNS network: %v.", err)
+		fmt.Printf("Failed to delete HNS network: %v.\n", err)
 	}
 }
 
 func CreateEndpoint(containerID string, isInfraContainer bool, endpointName string, networkName string, gateway string,
-	ipAddr string){
+	ipAddr string, isECSBridge bool) {
 	hnsEndpoint, err := hcsshim.GetHNSEndpointByName(endpointName)
 	if err == nil {
-		fmt.Printf("Found Existing Endpoint: %s", endpointName)
-		if !isInfraContainer{
+		fmt.Printf("Found Existing Endpoint: %s\n", endpointName)
+		if !isInfraContainer {
 			err := attachEndpoint(hnsEndpoint, containerID)
-			if err != nil{
-				fmt.Printf("Error while attaching endpoint")
+			fmt.Println("Attached this endpoint to container")
+			if err != nil {
+				fmt.Printf("Error while attaching endpoint\n")
 				return
 			}
 		}
 		return
 	}
-	fmt.Printf("Creating new endpoint with name %s", endpointName)
+	fmt.Printf("Creating new endpoint with name %s\n", endpointName)
 
-	DNSServers := [] string{"10.0.0.2"}
 	// Initialize the HNS endpoint.
 	hnsEndpoint = &hcsshim.HNSEndpoint{
 		Name:               endpointName,
 		VirtualNetworkName: networkName,
-		DNSServerList:      strings.Join(DNSServers, ","),
-		GatewayAddress:     gateway,
-		IPAddress:          net.ParseIP(ipAddr),
 	}
 
+	if !isECSBridge {
+		DNSServers := []string{"10.0.0.2"}
+		hnsEndpoint.DNSServerList = strings.Join(DNSServers, ",")
+		hnsEndpoint.DNSSuffix = strings.Join([]string{}, ",")
+		hnsEndpoint.GatewayAddress = gateway
+		hnsEndpoint.IPAddress = net.ParseIP(ipAddr)
+	}
 	// Attach policies
 	err = addEndpointPolicy(
 		hnsEndpoint,
-		hnsRoutePolicy{
-			Policy:            hcsshim.Policy{Type: hcsshim.Route},
-			DestinationPrefix: ipAddr + "/32",
-			NeedEncap:         true,
-		})
-	if err != nil {
-		fmt.Printf("Failed to add endpoint route policy: %v.", err)
-		return
-	}
-
-	err = addEndpointPolicy(
-		hnsEndpoint,
-		hnsRoutePolicy{
+		hcsshim.OutboundNatPolicy{
 			Policy: hcsshim.Policy{Type: hcsshim.OutboundNat},
+			// Implicit VIP: nw.ENIIPAddress.IP.String(),
+			Exceptions: []string{"10.0.0.0/16"},
 		})
 	if err != nil {
-		fmt.Printf("Failed to add OutboundNat policy: %v.", err)
+		fmt.Printf("Failed to add endpoint SNAT policy: %v.\n", err)
 		return
 	}
 
+	if !isECSBridge {
+		err = addEndpointPolicy(
+			hnsEndpoint,
+			hnsRoutePolicy{
+				Policy:            hcsshim.Policy{Type: hcsshim.Route},
+				DestinationPrefix: ipAddr + "/32",
+				NeedEncap:         true,
+			})
+		if err != nil {
+			fmt.Printf("Failed to add endpoint route policy: %v.\n", err)
+			return
+		}
+	}
 	//////////////////
 	// Encode the endpoint request.
 	buf, err := json.Marshal(hnsEndpoint)
@@ -126,23 +133,23 @@ func CreateEndpoint(containerID string, isInfraContainer bool, endpointName stri
 	hnsRequest := string(buf)
 
 	// Create the HNS endpoint.
-	fmt.Printf("Creating HNS endpoint: %+v", hnsRequest)
+	fmt.Printf("Creating HNS endpoint: %+v\n", hnsRequest)
 	hnsResponse, err := hcsshim.HNSEndpointRequest("POST", "", hnsRequest)
 	if err != nil {
-		fmt.Printf("Failed to create HNS endpoint: %v.", err)
+		fmt.Printf("Failed to create HNS endpoint: %v.\n", err)
 		return
 	}
 
-	fmt.Printf("Received HNS endpoint response: %+v.", hnsResponse)
+	fmt.Printf("Received HNS endpoint response: %+v.\n", hnsResponse)
 
 	// Attach the HNS endpoint to the container's network namespace.
 	err = attachEndpoint(hnsResponse, containerID)
 	if err != nil {
 		// Cleanup the failed endpoint.
-		fmt.Printf("Deleting the failed HNS endpoint %s.", hnsResponse.Id)
+		fmt.Printf("Deleting the failed HNS endpoint %s.\n", hnsResponse.Id)
 		_, delErr := hcsshim.HNSEndpointRequest("DELETE", hnsResponse.Id, "")
 		if delErr != nil {
-			fmt.Printf("Failed to delete HNS endpoint: %v.", delErr)
+			fmt.Printf("Failed to delete HNS endpoint: %v.\n", delErr)
 		}
 
 		return
@@ -150,22 +157,22 @@ func CreateEndpoint(containerID string, isInfraContainer bool, endpointName stri
 
 	// Return network interface MAC address.
 	mac, _ := net.ParseMAC(hnsResponse.MacAddress)
-	fmt.Printf("MAC Address is %v", mac)
-
+	fmt.Printf("MAC Address is %v and Gateway is %v\n", mac, hnsResponse.GatewayAddress)
+	fmt.Printf("Response is %v\n", hnsResponse)
 	return
 }
 
-func DeleteEndpoint(){
+func DeleteEndpoint() {
 
 }
 
 func attachEndpoint(ep *hcsshim.HNSEndpoint, containerID string) error {
-	fmt.Printf("Attaching HNS endpoint %s to container %s.", ep.Id, containerID)
+	fmt.Printf("Attaching HNS endpoint %s to container %s.\n", ep.Id, containerID)
 	err := hcsshim.HotAttachEndpoint(containerID, ep.Id)
 	if err != nil {
 		// Attach can fail if the container is no longer running and/or its network namespace
 		// has been cleaned up.
-		fmt.Printf("Failed to attach HNS endpoint %s: %v.", ep.Id, err)
+		fmt.Printf("Failed to attach HNS endpoint %s: %v.\n", ep.Id, err)
 	}
 
 	return err
@@ -174,7 +181,7 @@ func attachEndpoint(ep *hcsshim.HNSEndpoint, containerID string) error {
 func addEndpointPolicy(ep *hcsshim.HNSEndpoint, policy interface{}) error {
 	buf, err := json.Marshal(policy)
 	if err != nil {
-		fmt.Printf("Failed to encode policy: %v.", err)
+		fmt.Printf("Failed to encode policy: %v.\n", err)
 		return err
 	}
 
